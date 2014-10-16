9d54694a6b8c7e845a785a738cf185ace3107c5d
agsoares <agsoares@gmail.com>
2014-10-15 23:29:07 -0300

    Inicio do cadastro de pacientes

---
+++ cadastrarpaciente.php
@@ -0,0 +1,32 @@
+<!-- INCLUI O INICIO DO ARQUIVO -->
+<? include ("header.php"); ?>
+
+<!-- INICIO DO ARQUIVO -->
+<body class="metro">
+    
+    <br>
+
+    <img src="imagens/photo1.jpg" />
+    
+    <center><img src="imagens/principal1.png" /></center>
+    <br><br>
+    
+    <div class="grid">
+        <div class="row">
+    
+            <? include ("menu.php"); ?>
+            <?
+                
+                echo $path=$_SERVER['DOCUMENT_ROOT'];
+                
+            ?>
+            <br>
+            
+        </div>
+    </div>
+
+</body>
+<!-- FIM DO ARQUIVO -->
+
+<!-- INCLUI O FIM DO ARQUIVO -->
+<? include ("footer.php"); ?>
